2284cb72ed38d6112445b80d71d981227b54c652
Maxim Momotenko <yamahaped@gmail.com>
2023-03-09 13:56:14 +0100

    upload

---
+++ QuizApplication.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using QuizApplication.Domain.Questions;
+
+var question1 = new Question();
+var question2 = new Question("What is the capital of the UK?");
+var question3 = new Question("What is the capital of the USA?", "London", "New York", "Washington, D.C.", "San Diego", 'C');
+var question4 = new Question
+{
+    QuestionText = "What is the capital of the Ukraine?", OptionA = "Kyiv", OptionB = "Odesa", OptionC = "Lviv",
+    OptionD = "Dnepr", CorrectAnswerLetter = 'A'
+};
+
+var f = question4.AreOptionsValid();
+var e = question1.AreOptionsValid();
+Console.WriteLine();
---
+++ QuizApplication.Domain/Questions/Question.cs
@@ -0,0 +1,77 @@
+﻿namespace QuizApplication.Domain.Questions;
+
+public class Question
+{
+    public string? QuestionText;
+    public string? OptionA;
+    public string? OptionB;
+    public string? OptionC;
+    public string? OptionD;
+    public char? CorrectAnswerLetter;
+    private static char _defaultCorrectAnswerLetter = 'X';
+
+    public Question()
+    {
+        QuestionText = null;
+        OptionA = null;
+        OptionB = null;
+        OptionC = null;
+        OptionD = null;
+        CorrectAnswerLetter = _defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText)
+    {
+        QuestionText = questionText;
+        OptionA = null;
+        OptionB = null;
+        OptionC = null;
+        OptionD = null;
+        CorrectAnswerLetter = _defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText, string optionA, string optionB, string optionC, string optionD, char correctAnswerLetter)
+    {
+        QuestionText = questionText;
+        OptionA = optionA;
+        OptionB = optionB;
+        OptionC = optionC;
+        OptionD = optionD;
+
+        if (correctAnswerLetter is 'A' or 'B' or 'C' or 'D')
+        {
+            CorrectAnswerLetter = correctAnswerLetter;
+        }
+        else
+        {
+            CorrectAnswerLetter = _defaultCorrectAnswerLetter;
+        }
+    }
+
+    public bool AreOptionsValid()
+    {
+        var validOptions = 0;
+
+        if (OptionA != null)
+        {
+            validOptions += 1;
+        }
+
+        if (OptionB != null)
+        {
+            validOptions += 1;
+        }
+
+        if (OptionC != null)
+        {
+            validOptions += 1;
+        }
+
+        if (OptionC != null)
+        {
+            validOptions += 1;
+        }
+
+        return validOptions >= 2;
+    }
+}
